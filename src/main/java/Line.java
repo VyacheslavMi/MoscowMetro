f31abb37fd6a5d3b959da756fe3f782be4b23923
Vyacheslav <mihailov.ifix@gmail.com>
2020-05-05 04:38:54 +0300

    Update Line.java

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,22 +1,16 @@
 class Line {
     private String number;
     private String name;
-    public Line(){ }
-
+    public Line(String number, String name){
+        this.number = number;
+        this.name = name;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getNumber() {
         return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
     }
 }
